29218c7d3ae30b96cb34a9c3559cbb6a88ca349f
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-21 12:58:35 -0300

    data binding bidirecional

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,38 @@
 <script>
-	let name = "Helton";
-	let age = 0;
+  let name = "Helton";
+  let age = 0;
 
-	/* Variável reativa: altera seu valor assim que o valor atribuído é alterado
-	 * ou executado. Não é necessário declarar! O símbolo "$:" significa
-	 * basicamente: execute a operação assim que houver alguma modificação e/ou
-	 * execução.
-	*/
-	$: upperCaseName = name.toUpperCase();
-	$: console.log(name)
-	$: if (name == "Ricardo") {
-		age = 26;
-	}
+  /* Variável reativa: altera seu valor assim que o valor atribuído é alterado
+   * ou executado. Não é necessário declarar! O símbolo "$:" significa
+   * basicamente: execute a operação assim que houver alguma modificação e/ou
+   * execução.
+   */
+  $: upperCaseName = name.toUpperCase();
+  $: console.log(name);
+  $: if (name == "Ricardo") {
+    age = 26;
+  }
 
-	function incrementAge() {
-		age += 1;
-	}
+  function incrementAge() {
+    age += 1;
+  }
 
-	function changeName() {
-		name = "Ricardo";
-	}
+  function changeName() {
+    name = "Ricardo";
+  }
 
-	function nameInput(event) {
-		name = event.target.value;
-	}
-
+  function nameInput(event) {
+    name = event.target.value;
+  }
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
 <h1>My name is {upperCaseName} and I am {age} years old!</h1>
-<button on:click="{incrementAge}">Increment Age</button>
+<button on:click={incrementAge}>Increment Age</button>
 <!-- <button on:click="{changeName}">Change Name</button> -->
-<input type="text" value="{name}" on:input="{nameInput}">
+<input type="text" value={name} on:input={nameInput} />
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
