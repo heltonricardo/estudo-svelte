755f6959add318c9615e742d65489adedad36dec
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-21 13:30:06 -0300

    componente ContactCard

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+	import ContactCard from "./ContactCard.svelte"
+
   let name = "Helton";
   let age = 0;
 
@@ -31,6 +33,8 @@
 <!-- <button on:click="{changeName}">Change Name</button> -->
 <!-- <input type="text" value={name} on:input={nameInput} /> -->
 <input type="text" bind:value={name} />
+
+<ContactCard />
 
 <style>
   h1 {
