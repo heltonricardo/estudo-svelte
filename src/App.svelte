1dfc092faa5dd127454dd3ed09c1febcaa8adc55
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-21 12:01:58 -0300

    variavel reativa

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,18 @@
 	let name = "Helton";
 	let age = 0;
 
+	// Variável reativa: altera seu valor assim que o valor atribuído é alterado \
+	// ou executado. Não é necessário declarar!
+	$: upperCaseName = name.toUpperCase();
+
 	function incrementAge() {
 		age += 1;
 	}
+
+	function changeName() {
+		name = "Ricardo";
+	}
+
 </script>
 
 <style>
@@ -13,5 +22,6 @@
 	}
 </style>
 
-<h1>My name is {name} and I am {age} years old!</h1>
+<h1>My name is {upperCaseName} and I am {age} years old!</h1>
 <button on:click="{incrementAge}">Increment Age</button>
+<button on:click="{changeName}">Change Name</button>
