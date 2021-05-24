eab3d8ef27d247fdcd7eb12426beed879dff99e6
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-05-24 09:35:50 -0300

    adicionando varios cartoes

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,8 @@
   let description = "";
   let formState = "empty";
 
+  let createdContacts = [];
+
   function addContact() {
     if (
       name.trim().length == 0 ||
@@ -17,6 +19,17 @@
       formState = "invalid";
       return;
     }
+
+    createdContacts = [
+      ...createdContacts,
+      {
+        name,
+        jogTitle: title,
+        imageURL: image,
+        desc: description,
+      },
+    ];
+
     formState = "done";
   }
 </script>
@@ -42,18 +55,20 @@
 
 <button on:click={addContact}>Add Contact Card</button>
 
-{#if formState === "done"}
-  <ContactCard
-    userName={name}
-    jobTitle={title}
-    {description}
-    userImage={image}
-  />
-{:else if formState === "invalid"}
+{#if formState === "invalid"}
   <p>Invalid input.</p>
 {:else}
   <p>Please enter some data and hit the button!</p>
 {/if}
+
+{#each createdContacts as contact}
+  <ContactCard
+    userName={contact.name}
+    jobTitle={contact.jobTitle}
+    description={contact.desc}
+    userImage={contact.imageURL}
+  />
+{/each}
 
 <style>
   #form {
