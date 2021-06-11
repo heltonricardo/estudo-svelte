28361ba5fd95addaa0e6e37d6343afeb39b2783c
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-06-11 11:11:04 -0300

    template para estudo de transicao, movimento e animacao

--- src/App.svelte
+++ src/App.svelte
@@ -3,24 +3,38 @@
   import ComponentEvents from "./ComponentEvents/ComponentEvents.svelte";
   import BindingsAndForms from "./BindingsAndForms/BindingsAndForms.svelte";
   import DataStore from "./DataStore/DataStore.svelte";
-  let option = 1;
+  import Animations from "./Animations/Animations.svelte";
+
+  let option = 5;
+  $: console.log(option);
 </script>
 
+<style>
+  hr {
+    margin: 3rem 0;
+  }
+</style>
+
 <p>
-  Select the module: 
+  Select the module:
   <select bind:value={option}>
-    <option value=1>Basic</option>
-    <option value=2>Component Events</option>
-    <option value=3>Binding and Forms</option>
-    <option value=4>Data Store</option>
+    <option value={1}>Basic</option>
+    <option value={2}>Component Events</option>
+    <option value={3}>Binding and Forms</option>
+    <option value={4}>Data Store</option>
+    <option value={5}>Transitions, Motions and Animations</option>
   </select>
 </p>
-{#if option == 1}
+<hr />
+
+{#if option === 1}
   <Basic />
-{:else if option == 2}
+{:else if option === 2}
   <ComponentEvents />
-{:else if option == 3}
+{:else if option === 3}
   <BindingsAndForms />
-{:else if option == 4}
+{:else if option === 4}
   <DataStore />
+{:else if option === 5}
+  <Animations />
 {/if}
