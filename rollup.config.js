a27424e6c2317a043b9c764079259a51fee23b49
unknown <50843386+heltonricardo@users.noreply.github.com>
2021-06-14 22:50:22 -0300

    pasta docs voltou a ser public e remocao dos arquivos de build

--- rollup.config.js
+++ rollup.config.js
@@ -34,7 +34,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'docs/build/bundle.js'
+		file: 'public/build/bundle.js'
 	},
 	plugins: [
 		svelte({
@@ -62,9 +62,9 @@ export default {
 		// the bundle has been generated
 		!production && serve(),
 
-		// Watch the `docs` directory and refresh the
+		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('docs'),
+		!production && livereload('public'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
