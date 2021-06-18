f58e1583de3ffdef9fbaf421656d9442cfefa819
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2021-06-18 14:05:10 -0300

    componentes recursivos

---
+++ src/SpecialElements/RecursiveComponent/FamilyNode.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let member;
+</script>
+
+<style>
+  div {margin-left: 2rem;}
+</style>
+
+<div>
+  <h1>{member.name}</h1>
+  {#if member.isParent}
+    {#each member.children as child}
+      <svelte:self member={child} />
+    {/each}
+  {/if}
+</div>
+
+<!--
+  A tag svelte:self serve para renderizarmos um componente a partir dele mesmo.
+  Muito útil em estruturas hierárquicas como árvore de pastas, por exemplo, ou
+  um tipo de estrutura cujo o componente tenha dependência de si mesmo.
+-->
--- src/SpecialElements/SpecialElements.svelte
+++ src/SpecialElements/SpecialElements.svelte
@@ -1,6 +1,7 @@
 <script>
-  import Product from "./Product.svelte";
-  import CartItem from "./CartItem.svelte";
+  import Product from "./DynamicComponent/Product.svelte";
+  import CartItem from "./DynamicComponent/CartItem.svelte";
+  import FamilyNode from "./RecursiveComponent/FamilyNode.svelte";
 
   let list = [
     { component: Product, data: { id: "p1", title: "Product", price: 13.18 } },
@@ -11,12 +12,43 @@
   function toggle() {
     selected = selected ? 0 : 1;
   }
+
+  /********************/
+
+  let familyStructure = [
+    {
+      name: "John",
+      isParent: true,
+      children: [
+        {
+          name: "Moe",
+          isParent: true,
+          children: [{ name: "Julie", isParent: false }],
+        },
+      ],
+    },
+    { name: "Anna", isParent: false },
+  ];
 </script>
 
+<style>
+  hr {
+    margin: 3rem 0;
+  }
+</style>
+
+<!-- Anotação 01 -->
 <button on:click={toggle}>Toggle Display</button>
 <svelte:component this={list[selected].component} {...list[selected].data} />
 
+<hr />
+
+{#each familyStructure as familyMember}
+  <FamilyNode member={familyMember} />
+{/each}
+
 <!--
+  Anotação 01
   A tag svelte:component permite criar componentes informando o nome do
   construtor do componente através do this={} e passando os o valores das
   propriedades normalmente como já fizemos. Nesse caso, criamos objetos com o
